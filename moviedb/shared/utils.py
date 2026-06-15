bbda75dd5ebaff6d91fd6151a3b4a07f53fc9951
Max Hölzer <max.hoelzer@gmx.de>
2026-06-15 20:29:25 +0200

    Fixed uploading files.

--- moviedb/shared/utils.py
+++ moviedb/shared/utils.py
@@ -1,6 +1,5 @@
 from flask import current_app
 from werkzeug.utils import secure_filename
-import moviedb
 import pathlib
 import os
 from secrets import token_hex
@@ -8,8 +7,7 @@ from PIL import Image
 
 def upload_file(request, field_name):
     file = None
-    filename = None
-    save_filename = None
+    filename = None   
     root_path = None
 
     if field_name in request.files:
@@ -19,30 +17,16 @@ def upload_file(request, field_name):
         filename = secure_filename(file.filename)
         suffix= pathlib.Path(filename).suffix
         only_filename = token_hex(8) + suffix
-        save_filename = os.path.join(current_app.config['UPLOAD_FOLDER'], only_filename)
-        if current_app.config['DEBUG']:
-            root_path = os.path.join(current_app.root_path, save_filename)
+        if os.path.isabs(current_app.config['UPLOAD_FOLDER']):
+            root_path = os.path.join(current_app.config['UPLOAD_FOLDER'], only_filename)
         else:
-            root_path = save_filename
+            root_path = os.path.join(os.path.join(current_app.root_path, current_app.config['UPLOAD_FOLDER']), only_filename)
         file.save(root_path)
         new_image = Image.open(root_path)
         new_image.thumbnail((255, 255))
         new_image.save(root_path)
-        
-        new_path = os.path.normpath(save_filename)
-        splitted = new_path.split(os.sep)
 
-        link_name = None
-        index = 0
-
-        for i in range(len(splitted)):
-            if splitted[i] == 'uploads':
-                index = i
-                break
-
-        link_name = os.path.join(splitted[index], splitted[len(splitted) - 1])
-
-        return link_name
+        return os.path.join('uploads', only_filename)
     
     return None
 
@@ -50,10 +34,9 @@ def delete_old_file(filename):
     root_path = None
 
     if filename is not None:
-        if current_app.config['DEBUG']:
-            root_path = os.path.join(current_app.root_path, filename)
+        if os.path.isabs(current_app.config['UPLOAD_FOLDER']):
+            root_path = os.path.join(current_app.config['UPLOAD_FOLDER'], str(filename).split(os.sep)[1])
         else:
-            splitted = str(filename).split(os.sep)
-            root_path = os.path.join(current_app.config['UPLOAD_FOLDER'], splitted[len(splitted) - 1])
+            root_path = os.path.join(os.path.join(current_app.root_path, current_app.config['UPLOAD_FOLDER']), str(filename).split(os.sep)[1])
         if os.path.exists(root_path):
             os.remove(root_path)
