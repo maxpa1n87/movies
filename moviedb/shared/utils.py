2813933c6006abc05616cc2e1b672210d99653fa
Max Hölzer <max.hoelzer@gmx.de>
2026-06-21 18:41:39 +0200

    Changed root_path to full_path

--- moviedb/shared/utils.py
+++ moviedb/shared/utils.py
@@ -9,7 +9,7 @@ from werkzeug.exceptions import RequestEntityTooLarge
 def upload_file(request, field_name):
     file = None
     filename = None   
-    root_path = None
+    full_path = None
     file_size = 0
 
     if field_name in request.files:
@@ -26,14 +26,14 @@ def upload_file(request, field_name):
         suffix= pathlib.Path(filename).suffix
         only_filename = token_hex() + suffix
         if os.path.isabs(current_app.config['UPLOAD_FOLDER']):
-            root_path = os.path.join(current_app.config['UPLOAD_FOLDER'], only_filename)
+            full_path = os.path.join(current_app.config['UPLOAD_FOLDER'], only_filename)
         else:
-            root_path = os.path.join(os.path.join(current_app.root_path, current_app.config['UPLOAD_FOLDER']), only_filename)
-        file.save(root_path, current_app.config['MAX_BUFFER_SIZE_FOR_FILE'])
-        new_image = Image.open(root_path)
+            full_path = os.path.join(os.path.join(current_app.root_path, current_app.config['UPLOAD_FOLDER']), only_filename)
+        file.save(full_path, current_app.config['MAX_BUFFER_SIZE_FOR_FILE'])
+        new_image = Image.open(full_path)
         fixed_image = ImageOps.exif_transpose(new_image)
         fixed_image.thumbnail((255, 255))
-        fixed_image.save(root_path)
+        fixed_image.save(full_path)
         new_image.close()
         fixed_image.close()
         return only_filename
@@ -41,12 +41,12 @@ def upload_file(request, field_name):
     return None
 
 def delete_old_file(filename):
-    root_path = None
+    full_path = None
 
     if filename is not None:
         if os.path.isabs(current_app.config['UPLOAD_FOLDER']):
-            root_path = os.path.join(current_app.config['UPLOAD_FOLDER'], filename)
+            full_path = os.path.join(current_app.config['UPLOAD_FOLDER'], filename)
         else:
-            root_path = os.path.join(os.path.join(current_app.root_path, current_app.config['UPLOAD_FOLDER']), filename)
-        if os.path.exists(root_path):
-            os.remove(root_path)
+            full_path = os.path.join(os.path.join(current_app.root_path, current_app.config['UPLOAD_FOLDER']), filename)
+        if os.path.exists(full_path):
+            os.remove(full_path)
