ae637214732c0df90af4689b0fe8e0e85ed4485a
Max Hölzer <max.hoelzer@gmx.de>
2026-06-26 21:17:47 +0200

    Fixed download_file function.

--- moviedb/download.py
+++ moviedb/download.py
@@ -1,7 +1,15 @@
 from flask import Blueprint, current_app, send_from_directory
+import os
 
 bp = Blueprint('download', __name__)
 
 @bp.route('/uploads/<name>')
 def download_file(name):
-    return send_from_directory(current_app.config['UPLOAD_FOLDER'], name)
+    full_path = None
+
+    if os.path.isabs(current_app.config['UPLOAD_FOLDER']):
+        full_path = current_app.config['UPLOAD_FOLDER']
+    else:
+        full_path = os.path.join(current_app.instance_path, current_app.config['UPLOAD_FOLDER'])
+
+    return send_from_directory(full_path, name)
