8ae136de2e61fcd5562bff00cfbc1b54d678db25
Max Hölzer <max.hoelzer@gmx.de>
2026-06-26 20:03:44 +0200

    Revert "Fixed upload and delete file functions."
    This reverts commit 857dcf9872e7970f3ebe5712aeaa3b23101b7290.
    
    Reverting commit fixing file uploads and delete file functions.

--- moviedb/shared/utils.py
+++ moviedb/shared/utils.py
@@ -25,9 +25,10 @@ def upload_file(request, field_name):
         filename = secure_filename(file.filename)
         suffix= pathlib.Path(filename).suffix
         only_filename = token_hex() + suffix
-        
-        full_path = os.path.join(current_app.config['UPLOAD_FOLDER'], only_filename)
-
+        if os.path.isabs(current_app.config['UPLOAD_FOLDER']):
+            full_path = os.path.join(current_app.config['UPLOAD_FOLDER'], only_filename)
+        else:
+            full_path = os.path.join(os.path.join(current_app.root_path, current_app.config['UPLOAD_FOLDER']), only_filename)
         file.save(full_path, current_app.config['MAX_BUFFER_SIZE_FOR_FILE'])
         new_image = Image.open(full_path)
         fixed_image = ImageOps.exif_transpose(new_image)
@@ -35,7 +36,6 @@ def upload_file(request, field_name):
         fixed_image.save(full_path)
         new_image.close()
         fixed_image.close()
-
         return only_filename
     
     return None
@@ -44,8 +44,9 @@ def delete_old_file(filename):
     full_path = None
 
     if filename is not None:
-        
-        full_path = os.path.join(current_app.config['UPLOAD_FOLDER'], filename)
-        
+        if os.path.isabs(current_app.config['UPLOAD_FOLDER']):
+            full_path = os.path.join(current_app.config['UPLOAD_FOLDER'], filename)
+        else:
+            full_path = os.path.join(os.path.join(current_app.root_path, current_app.config['UPLOAD_FOLDER']), filename)
         if os.path.exists(full_path):
             os.remove(full_path)
